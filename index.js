fa02cfbf7ea7e45949cedd9bc08aee09767b1e1f
Tushar12152 <tusharimran470@gmail.com>
2023-10-29 23:34:50 -0600

    token added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const jwt=require("jsonwebtoken")
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config()
 const app=express()
@@ -7,8 +9,12 @@ const port=process.env.PORT || 5002;
 
 
 //middleware
-app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
+app.use(cors({
+   origin:["http://localhost:5173"],
+   credentials:true,
+}))
 
 
 
@@ -18,7 +24,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tgzt8q2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -61,10 +67,104 @@ async function run() {
            res.send(result)
      })
 
+  // get single product by id
+
+  app.get("/products/:id",verify, async(req,res)=>{
+        try{
+          const id=req.params.id;
+        const query= {_id:new ObjectId(id)}
+        const result=await productCollection. findOne(query)
+        res.send(result)
+        }catch(err){
+          console.log(err);
+        }
+  })
+
+
+  //update data
+
+  //  app.put('/products/:id',async(req,res)=>{
+  //       const id=req.params.id;
+  //       const body =req.body;
+  //       console.log(body);
+
+  //        const updatedData={
+  //         $set:{
+  //              ...body,
+  //         }
+  //        }
+
+  //        const option={upsert:true}
+  //        const result=await productCollection.updateOne(id,updatedData,option)
+  //        res.send(result)
+  //  })
+
+
+
+  app.put('/products/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    console.log(body);
+
+    const filter = { _id:new ObjectId(id) }; // Creating a filter using the provided ID
+
+    const updatedData = {
+        $set: {
+            ...body,
+        }
+    }
+
+    const option = { upsert: true }
+
+    try {
+        const result = await productCollection.updateOne(filter, updatedData, option);
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 
 
 
+     app.delete("/products/:id",async(req,res)=>{
+
+      try{
+        const id=req.params.id;
+        //  console.log(id)
+         const query={_id:new ObjectId(id)}
+         const result= await productCollection.deleteOne(query)
+         res.send(result)
+      }
+      catch(err){
+    console.log(err)
+      }
+        
+     })
+
+
+
+     app.post('/jwt',async(req,res)=>{
+         const data=req.body;
+        //  console.log(data)
+        const token=jwt.sign(data,process.env.JWT_SECRET,{expiresIn:"1h"})
+
+           
+
+        const expirationDate = new Date(); // Create a new date object
+        expirationDate.setDate(expirationDate.getDate() + 7); // Set it to expire 7 days from now
+
+         res
+         .cookie("token",token,{
+          httpOnly:true,
+          secure:false,
+          expires:expirationDate,
+         })
+         .send ({msg:"succese",})
+     })
+
+    
 
 
 
@@ -82,7 +182,23 @@ run().catch(console.dir);
 
 
 
+const verify= async(req,res,next)=>{
+     const token=req.cookies?.token;
+     if(!token){
+      res.status(401).send({status:"unAuthorized Access", code:401})
+      return;
+     }
 
+     jwt.verify(token,process.env.JWT_SECRET,(err,decode)=>{
+          if(err){
+            res.status(401).send({status:"unAuthorized Access", code:401})
+          } else{
+            console.log(decode);
+          }
+     })
+
+     next()
+}
 
 
 
